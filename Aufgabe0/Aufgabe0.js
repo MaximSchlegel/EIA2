2ee85aac8be24fc027bf75e9a48e614159230409
Maxim Schlegel <Maxim Schlegel@DESKTOP-3RASVRT.mshome.net>
2018-03-24 22:30:11 +0100

    Aufgabe 0

---
+++ Aufgabe0/Aufgabe0.js
@@ -0,0 +1,9 @@
+var Aufgabe0;
+(function (Aufgabe0) {
+    document.addEventListener('DOMContentLoaded', function () {
+        var name;
+        name = prompt("Bitte Namen eingeben");
+        document.getElementById("greeting").innerHTML += name;
+    });
+})(Aufgabe0 || (Aufgabe0 = {}));
+//# sourceMappingURL=Aufgabe0.js.map
---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,11 @@
+namespace Aufgabe0 {
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        var name:string;
+        name = prompt("Bitte Namen eingeben");
+        document.getElementById("greeting").innerHTML += name;
+        });
+}
+
+
+
