70f184b9bb00503e27828c9a9c145ebe298e0027
Maxim Schlegel <Maxim Schlegel@DESKTOP-3RASVRT.mshome.net>
2018-04-20 20:53:54 +0200

    Noch unfertige Aufgabe gepusht

---
+++ Aufgabe3/Aufgabe3.js
@@ -0,0 +1,70 @@
+var Aufgabe2;
+(function (Aufgabe2) {
+    var numPairs;
+    var numPlayers = 1;
+    var cardContent = ["A", "B", "C", "D", "E", "F", "G", "H"];
+    var cardArray = [];
+    var cardOpenArray = [];
+    var status = "hidden";
+    var cardsOpen = 0;
+    function createCard(_cardContent, _status) {
+        var card = document.createElement("div");
+        card.innerHTML = "<p>" + _cardContent + "</p>";
+        card.setAttribute("class", "card hidden");
+        cardArray.push(card);
+        card.addEventListener("click", clickHandler);
+    }
+    function clickHandler(_event) {
+        var target = _event.target;
+        if (target.classList.contains("card")) {
+            cardsOpen++;
+            if (cardsOpen <= 2) {
+                if (target.classList.contains("hidden")) {
+                    target.classList.remove("hidden");
+                    target.classList.add("open");
+                }
+            }
+            else if (cardsOpen == 2) {
+                setTimeout(compareCards(), 2000);
+            }
+        }
+    }
+    function compareCards() {
+        for (var i = 0; i < 2; i++) {
+            if (cardOpenArray[0].innerHTML == cardOpenArray[1].innerHTML) {
+                cardOpenArray[i].classList.remove("hidden");
+                cardOpenArray[i].classList.add("taken");
+            }
+            else {
+                cardOpenArray[i].classList.remove("open");
+                cardOpenArray[i].classList.add("hidden");
+            }
+        }
+    }
+    console.log(cardOpenArray);
+    function shuffleArray(_array) {
+        for (var i = _array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = _array[i];
+            _array[i] = _array[j];
+            _array[j] = temp;
+        }
+        return _array;
+    }
+    function main() {
+        numPairs = parseInt(prompt("Anzahl der Kartenpaare", "5-8"), 10);
+        if (numPairs < 5 || numPairs > 8) {
+            numPairs = 8;
+        }
+        for (var i = 0; i < numPairs; i++) {
+            createCard(cardContent[i], status);
+            createCard(cardContent[i], status);
+        }
+        shuffleArray(cardArray);
+        for (var i = 0; i < cardArray.length; i++) {
+            document.getElementById("cardBox").appendChild(cardArray[i]);
+        }
+    }
+    document.addEventListener("DOMContentLoaded", main);
+})(Aufgabe2 || (Aufgabe2 = {}));
+//# sourceMappingURL=Aufgabe3.js.map
