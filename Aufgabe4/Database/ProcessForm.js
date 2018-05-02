a8b11ab81a4f303900fe70554a0251551b267b35
Maxim Schlegel <Maxim Schlegel@DESKTOP-3RASVRT.mshome.net>
2018-05-02 13:03:19 +0200

    Code für Aufgabe 4 in eigenen Ordner kopiert

---
+++ Aufgabe4/Database/ProcessForm.js
@@ -0,0 +1,68 @@
+var L04_Interfaces;
+(function (L04_Interfaces) {
+    window.addEventListener("load", init);
+    var inputs = document.getElementsByTagName("input");
+    function init(_event) {
+        console.log("Init");
+        var insertButton = document.getElementById("insert");
+        var refreshButton = document.getElementById("refresh");
+        var searchButton = document.getElementById("search");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+        searchButton.addEventListener("click", search);
+    }
+    function insert(_event) {
+        var genderButton = document.getElementById("male");
+        var matrikel = inputs[2].value;
+        var studi;
+        studi = {
+            name: inputs[0].value,
+            firstname: inputs[1].value,
+            matrikel: parseInt(matrikel),
+            course: document.getElementsByTagName("select").item(0).value,
+            age: parseInt(inputs[3].value),
+            gender: genderButton.checked
+        };
+        console.log(studi);
+        console.log(studi.age);
+        console.log(studi["age"]);
+        // Datensatz im assoziativen Array unter der Matrikelnummer speichern
+        L04_Interfaces.studiHomoAssoc[matrikel] = studi;
+        // nur um das auch noch zu zeigen...
+        L04_Interfaces.studiSimpleArray.push(studi);
+    }
+    function refresh(_event) {
+        var output = document.getElementsByTagName("textarea")[0];
+        output.value = "";
+        // for-in-Schleife iteriert über die Schlüssel des assoziativen Arrays
+        for (var matrikel in L04_Interfaces.studiHomoAssoc) {
+            var studi = L04_Interfaces.studiHomoAssoc[matrikel];
+            var line = matrikel + " " + studi.course + ": ";
+            line += studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            output.value += line + "\n";
+        }
+    }
+    function search(_event) {
+        var matrikel = inputs[6].value;
+        var studi = L04_Interfaces.studiHomoAssoc[matrikel];
+        var output2 = document.getElementsByTagName("textarea")[1];
+        if (studi) {
+            var line = matrikel + ": ";
+            line += studi.course + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            output2.value = line;
+        }
+        else {
+            output2.value = "No Match";
+        }
+    }
+    // zusätzliche Konsolenausgaben zur Demonstration
+    console.group("Simple Array");
+    console.log(L04_Interfaces.studiSimpleArray);
+    console.groupEnd();
+    console.group("Associatives Array (Object)");
+    console.log(L04_Interfaces.studiHomoAssoc);
+    console.groupEnd();
+})(L04_Interfaces || (L04_Interfaces = {}));
+//# sourceMappingURL=ProcessForm.js.map
