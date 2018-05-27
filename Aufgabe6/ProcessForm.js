f7d5014a1e641b0ec0a05090826099ffb9a2c2b6
Maxim <Maxim@DESKTOP-V7IL6S5.local>
2018-05-27 12:44:13 +0200

    Aufgabe 6 erster Push

---
+++ Aufgabe6/ProcessForm.js
@@ -0,0 +1,78 @@
+/*
+Aufgabe: 6
+Name: Maxim Schlegel
+Matrikel: 257202
+Datum: 27.05.18
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+*/
+var Aufgabe6;
+(function (Aufgabe6) {
+    window.addEventListener("load", init);
+    var address = "https://eia2-node-danielkern.herokuapp.com";
+    var inputs = document.getElementsByTagName("input");
+    function init(_event) {
+        console.log("Init");
+        var insertButton = document.getElementById("insert");
+        var refreshButton = document.getElementById("refresh");
+        var searchButton = document.getElementById("search");
+        insertButton.addEventListener("click", insert);
+        refreshButton.addEventListener("click", refresh);
+        searchButton.addEventListener("click", search);
+    }
+    function insert(_event) {
+        var genderButton = document.getElementById("male");
+        var matrikel = inputs[2].value;
+        var studi;
+        studi = {
+            name: inputs[0].value,
+            firstname: inputs[1].value,
+            matrikel: parseInt(matrikel),
+            course: document.getElementsByTagName("select").item(0).value,
+            age: parseInt(inputs[3].value),
+            gender: genderButton.checked
+        };
+        var convert = JSON.stringify(studi);
+        console.log(convert);
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=insert&data=" + convert, true);
+        xhr.addEventListener("readystatechange", handleStateChangeInsert);
+        xhr.send();
+    }
+    function handleStateChangeInsert(_event) {
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            alert(xhr.response);
+        }
+    }
+    function refresh(_event) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=refresh", true);
+        xhr.addEventListener("readystatechange", handleStateChangeRefresh);
+        xhr.send();
+    }
+    function handleStateChangeRefresh(_event) {
+        var output = document.getElementsByTagName("textarea")[0];
+        output.value = "";
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            output.value += xhr.response;
+        }
+    }
+    function search(_event) {
+        var mtrkl = inputs[6].value;
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", address + "?command=search&searchFor=" + mtrkl, true);
+        xhr.addEventListener("readystatechange", handleStateChangeSearch);
+        xhr.send();
+    }
+    function handleStateChangeSearch(_event) {
+        var output = document.getElementsByTagName("textarea")[1];
+        output.value = "";
+        var xhr = _event.target;
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            output.value += xhr.response;
+        }
+    }
+})(Aufgabe6 || (Aufgabe6 = {}));
+//# sourceMappingURL=ProcessForm.js.map
