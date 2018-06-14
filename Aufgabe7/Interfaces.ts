e412398e26c8fd0c5b7eeeb41b911f341cbf17bd
Maxim <Maxim@DESKTOP-V7IL6S5.eduroam.hs-furtwangen.de>
2018-06-14 11:21:16 +0200

    Aufgabe 7

---
+++ Aufgabe7/Interfaces.ts
@@ -0,0 +1,20 @@
+namespace Aufgabe8 {
+    // Struktur des heterogenen assoziativen Arrays als Datensatz für eine studierende Person
+    export interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        course: string;
+        age: number;
+        gender: boolean;
+    }
+
+    // Struktur des homogenen assoziativen Arrays, bei dem ein Datensatz der Matrikelnummer zugeordnet ist
+    export interface Studis {
+         [matrikel: string]: Studi;
+    }
+    
+    // Homogenes assoziatives Array zur Speicherung einer Person unter der Matrikelnummer
+    export let studiHomoAssoc: Studis = {};
+    
+}
